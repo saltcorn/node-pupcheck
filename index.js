71a57f76b2616e72412e357e8bf79f0a164b8798
Tom Nielsen <tanielsen@gmail.com>
2022-10-13 12:02:47 +0100

    check, clean

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const Browser = require("./browser")
-const run = require("./run")
-const go = async () => {
-    const executablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+const run = require("./run");
 
-    //const b = await Browser.create({ executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' })
+(async () => {
+    const executablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
     //const executablePath = '/Applications/Chromium.app/Contents/MacOS/Chromium' 
+
     await run([
         {
             type: "goto",
@@ -20,6 +19,4 @@ const go = async () => {
         }
 
     ], { executablePath })
-}
-
-go()
+})()
--- run.js
+++ run.js
@@ -4,21 +4,29 @@ const Browser = require("./browser")
 module.exports = async (spec, options) => {
     if (!Array.isArray(spec)) throw new Error("pupcheck spec must be an array, got type: " + typeof spec)
     const b = await Browser.create(options)
-    for (const item of spec) {
-        await ({
-            async goto({ url }) {
-                await b.goto(url)
-            },
-            async contains({ text }) {
-                if (!(await b.content()).includes(text))
-                    throw new Error("contains not found:" + text)
-            },
-            async containsnot({ text }) {
-                if ((await b.content()).includes(text))
-                    throw new Error("containsnot found:" + text)
-            }
 
-        })[item.type](item)
+    const dispatch = {
+        async goto({ url }) {
+            await b.goto(url)
+        },
+        async contains({ text }) {
+            if (!(await b.content()).includes(text))
+                throw new Error("contains not found:" + text)
+        },
+        async containsnot({ text }) {
+            if ((await b.content()).includes(text))
+                throw new Error("containsnot found:" + text)
+        }
+
     }
+    for (const item of spec) {
+        if (!item.type) throw new Error("item has no type: " + JSON.stringify(item))
+        if (!Object.keys(dispatch).includes(item.type))
+            throw new Error("type not supported " + item.type)
+    }
+    for (const item of spec) {
+        await dispatch[item.type](item)
+    }
+    console.log(`${spec.length} steps passed`)
     await b.close()
 }

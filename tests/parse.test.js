4fc9a09640d2047852bb5d5329b41c37fe5c0350
Tom Nielsen <tanielsen@gmail.com>
2022-12-14 14:26:00 +0000

    fix some tests

--- tests/parse.test.js
+++ tests/parse.test.js
@@ -12,13 +12,27 @@ describe("text parser", () => {
   });
   it("parse simple file", async () => {
     expect(
-      parse(`
-          goto https://saltcorn.com
-          status 200        
-        `)
+      parse(
+        `goto https://saltcorn.com
+status 200        
+        `,
+        "myfile"
+      )
     ).toStrictEqual([
-      { type: "goto", url: "https://saltcorn.com" },
-      { type: "status", code: 200 },
+      {
+        fileName: "myfile",
+        line: "goto https://saltcorn.com",
+        lineNumber: 1,
+        type: "goto",
+        url: "https://saltcorn.com",
+      },
+      {
+        fileName: "myfile",
+        lineNumber: 2,
+        line: "status 200",
+        type: "status",
+        code: 200,
+      },
     ]);
   });
   it("parse file with comments", async () => {
@@ -29,28 +43,79 @@ describe("text parser", () => {
           status 200        
         `)
     ).toStrictEqual([
-      { type: "goto", url: "https://saltcorn.com" },
-      { type: "status", code: 200 },
+      {
+        fileName: undefined,
+        line: "goto https://saltcorn.com",
+        lineNumber: 2,
+        type: "goto",
+        url: "https://saltcorn.com",
+      },
+      {
+        fileName: undefined,
+        lineNumber: 4,
+        line: "status 200",
+        type: "status",
+        code: 200,
+      },
     ]);
   });
   it("parse click", async () => {
     expect(parse(`click #foo`)).toStrictEqual([
-      { type: "click", selector: "#foo" },
+      {
+        fileName: undefined,
+        line: "click #foo",
+        lineNumber: 1,
+        type: "click",
+        selector: "#foo",
+      },
     ]);
   });
   it("parse click with parens", async () => {
     expect(parse(`click (#foo a)`)).toStrictEqual([
-      { type: "click", selector: "#foo a" },
+      {
+        fileName: undefined,
+        line: "click (#foo a)",
+        lineNumber: 1,
+        type: "click",
+        selector: "#foo a",
+      },
+    ]);
+  });
+  it("parse another click with parens", async () => {
+    expect(
+      parse(`click (tr[data-todo-type=RECOMMENDATIONS] td:nth-child(2) a)`)
+    ).toStrictEqual([
+      {
+        fileName: undefined,
+        line: "click (tr[data-todo-type=RECOMMENDATIONS] td:nth-child(2) a)",
+        lineNumber: 1,
+        type: "click",
+        selector: "tr[data-todo-type=RECOMMENDATIONS] td:nth-child(2) a",
+      },
     ]);
   });
   it("parse simple type", async () => {
     expect(parse(`type #foo bar baz`)).toStrictEqual([
-      { type: "type", selector: "#foo", text: "bar baz" },
+      {
+        fileName: undefined,
+        line: "type #foo bar baz",
+        lineNumber: 1,
+        type: "type",
+        selector: "#foo",
+        text: "bar baz",
+      },
     ]);
   });
   it("parse type with child elem", async () => {
     expect(parse(`type (#foo input) bar baz`)).toStrictEqual([
-      { type: "type", selector: "#foo input", text: "bar baz" },
+      {
+        fileName: undefined,
+        line: "type (#foo input) bar baz",
+        lineNumber: 1,
+        type: "type",
+        selector: "#foo input",
+        text: "bar baz",
+      },
     ]);
   });
 });
